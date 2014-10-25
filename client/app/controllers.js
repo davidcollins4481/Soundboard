9578938307ff23f4e55d7095d5118cd83e97909a
David Collins <davidcollins4481@gmail.com>
2014-10-25 02:28:23 -0400

    .

---
+++ client/app/controllers.js
@@ -0,0 +1,44 @@
+var soundboardControllers = angular.module('soundboardControllers', []);
+
+soundboardControllers.controller('SoundboardController',
+    function($scope, $http, $routeParams) {
+        $scope.audioRootDirectory = '/sounds/';
+        $http.get('/getsounds')
+            .success(function(data) {
+                $scope.sounds = data;
+            });
+
+        $scope.toggle = function(obj, $event) {
+            console.log($event);
+            var button = $event.currentTarget;
+            var audio = button.nextElementSibling;
+
+            if (audio.className == "stopped") {
+                audio.play();
+                audio.className = "";
+                button.src = "/img/stop.png";
+            } else {
+                audio.pause();
+                audio.className = "stopped";
+                button.src = "/img/play.png";
+            }
+        }
+
+        $scope.delete = function(e) {
+            e.preventDefault();
+            console.log('deleting ' + this.sound.name);
+            $http.post('/deletesound',
+                    {filename: this.sound.name}
+                )
+                .success(function(data) {
+                    $scope.sounds = data;
+                    console.log('deleted');
+                })
+                .error(function() {
+                    console.log('error');
+                });
+        }
+
+
+    }
+);
--- client/app/soundboardApp.js
+++ client/app/soundboardApp.js
@@ -1,68 +1,15 @@
-var soundboardApp = angular.module('soundboardApp', []);
+var soundboardApp = angular.module('soundboardApp', ['ngRoute', 'soundboardControllers']);
 
-
-
-soundboardApp.controller('soundboardUploader', function ($scope, $http) {
-    $scope.uploading = [];
-
-    $scope.$on('filesDragged', function (event, files) {
-
-        var data = new FormData();
-        for (var i = 0; i < files.length; i++) {
-            data.append('file', files[i]);
+soundboardApp
+    .config(['$routeProvider',
+        function($routeProvider) {
+            $routeProvider.
+                when('/', {
+                    templateUrl: '/js/app/views/soundboard.html',
+                    controller: 'SoundboardController'
+                }).
+                otherwise({
+                    redirectTo: '/'
+                });
         }
-
-        $http.post('/upload', data, {
-            transformRequest: angular.identity,
-            headers: {'Content-Type': undefined}
-        })
-        .success(function(data) {
-            console.log('success');
-            $scope.sounds = data;
-        })
-        .error(function() {
-            console.log('fail');
-        });
-
-        $scope.$apply(function(){
-            $scope.uploading = files;
-        });
-    });
-});
-
-soundboardApp.directive('dropTarget', function(){
-    return {
-        scope: true,
-        link: function($scope, $element) {
-            $element.bind('dragover', function(e) {
-                console.log('drag');
-                e.stopPropagation();
-                e.preventDefault();
-
-            });
-
-            $element.bind('dragenter', function(e) {
-                console.log('dragenter');
-                e.stopPropagation();
-                e.preventDefault();
-                e.currentTarget.classList.add('active');
-            });
-
-            $element.bind('drop', function(e){
-                e.stopPropagation();
-                e.preventDefault();
-                e.currentTarget.classList.remove('active');
-                var files = e.dataTransfer.files;
-                $scope.$emit('filesDragged', files);
-                return false;
-            });
-
-            $element.bind('dragleave', function(e){
-                console.log('dropping');
-                e.stopPropagation();
-                e.preventDefault();
-                e.currentTarget.classList.remove('active');
-            });
-        }
-    }
-});
+    ]);
